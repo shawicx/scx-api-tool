78cbd50a4a9ac7751b3c13c6a197717a3b35c507
shawicx <d35f3153@proton.me>
2025-08-24 01:45:31 +0800

    chore: build opts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @scxfe/api-tool
 
+## 0.1.3
+
+### Patch Changes
+
+- fix: build opts
+
 ## 0.1.2
 
 ### Patch Changes
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,7 @@
  * @Author: shawicx d35f3153@proton.me
  * @Date: 2025-08-07 22:13:56
  * @LastEditors: shawicx d35f3153@proton.me
- * @LastEditTime: 2025-08-24 01:32:08
+ * @LastEditTime: 2025-08-24 01:44:14
  * @Description: tsdown 配置
  */
 import { defineConfig } from 'tsdown';
@@ -15,6 +15,11 @@ export default defineConfig([
     outDir: 'dist',
     dts: true,
     copy: ['src/templates/'],
+    minify: true,
+    sourcemap: false,
+    treeshake: true,
+    splitting: false,
+    external: ['typescript', 'prettier', 'babel'],
   },
   {
     entry: ['src/**/*.ts', '!src/service/**/*', '!src/templates/', '!apiPower.config.ts'],
@@ -23,5 +28,9 @@ export default defineConfig([
     outDir: 'lib',
     dts: true,
     copy: ['src/templates/'],
+    minify: true,
+    sourcemap: false,
+    treeshake: true,
+    external: ['typescript', 'prettier', 'babel'],
   },
 ]);
